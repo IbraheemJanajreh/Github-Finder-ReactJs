9b45693259069ffa8ff0ebd68554909cf2a7955d
Ibraheem <rf.nadal.22@gmail.com>
2022-07-27 13:49:41 +0300

    update token

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,9 +7,7 @@ const About = () => {
       <p className="mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details.
       </p>
-      <p className="text-lg text-gray-400">
-        Version <span className="text-white">1.0.0</span>
-      </p>
+      <p className="mb-4 text-2xl font-light">By: Ibraheem Janajreh</p>
     </>
   );
 };
